d6953c60841573104272e06382df6bdee485a255
Chumpa <iliya.kuznetsov@gmail.com>
2013-11-18 14:36:19 +0400

    Just to store

---
+++ pure/src/Test.java
@@ -0,0 +1,16 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+    }
+
+}
